02fb7e3bc4a4fa54923d7c9763ae3d3f8169ba86
Tulio Vitor Machado Faria <tuliofaria@gmail.com>
2020-10-20 17:12:04 -0300

    remove css unused

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,25 @@
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const isDev = NODE_ENV === 'development'
+
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer'],
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    !isDev
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/*.js',
+              './components/*.js',
+              './atoms/*.js',
+              './molecules/*.js',
+              './layouts/*.js',
+            ],
+            defaultExtractor: content =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          },
+        ]
+      : null,
+  ],
 }
