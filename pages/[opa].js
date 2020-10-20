74cc5c39aeb35ad01ac09aed58dec878724c49f6
Tulio Vitor Machado Faria <tuliofaria@gmail.com>
2020-10-20 17:29:52 -0300

    fix seo

--- pages/[opa].js
+++ pages/[opa].js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import tw from 'twin.macro'
+import Head from 'next/head'
 
 const Card2 = styled.div`
   ${tw`mx-2 mb-4 p-6 overflow-hidden bg-white shadow-lg`};
@@ -12,6 +13,10 @@ const Opa = () => {
   const router = useRouter()
   return (
     <>
+      <Head>
+        <title>Test Styled-components</title>
+        <meta name='description' content='This is a description' />
+      </Head>
       <h1>Test</h1>
       <Card2 rounded={router.query.opa === 'tulio'} />
     </>
